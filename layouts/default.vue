a29e211622b7d1f64cdba6f401bd416016fddb40
k-teraoka <k-teraoka@labol.co.jp>
2025-07-03 15:52:01 +0900

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <UApp class="grid min-h-screen grid-rows-[auto_1fr_auto]">
+    <header class="p-4 border-b border-gray-800">
+      <span class="font-bold text-xl">SUSPENSE SAMPLE</span>
+    </header>
+
+    <NuxtContainer class="py-8">
+      <slot />
+    </NuxtContainer>
+
+    <footer class="p-4 text-center text-sm text-gray-500">
+      &copy; 2025 MyApp. All rights reserved.
+    </footer>
+  </UApp>
+</template>
