ab64f744895266cdff8d0839d5cf90ae567487f8
k-teraoka <k-teraoka@labol.co.jp>
2025-07-08 15:14:38 +0900

    修正

--- components/UserPostImages.vue
+++ components/UserPostImages.vue
@@ -13,5 +13,6 @@
 import { fetchUserPostImages } from '~/api/user';
 import ImageGrid from '~/components/ImageGrid.vue';
 
-const images = await fetchUserPostImages() // 4000ms遅延
+const images = ref<string[]>([])
+images.value = await fetchUserPostImages() // 4000ms遅延
 </script>
