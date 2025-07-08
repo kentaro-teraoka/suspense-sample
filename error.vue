2097fad56cd54d71fdbd38de46703021844c37d1
k-teraoka <k-teraoka@labol.co.jp>
2025-07-08 14:39:15 +0900

    layout

--- error.vue
+++ error.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="min-h-screen flex flex-col gap-10 items-center justify-center">
-    <h1 class="text-8xl font-bold text-error-500">
-      {{ error?.statusCode }}
-    </h1>
-    <p class="text-xl text-gray-500">
-      {{ error?.statusMessage || '予期せぬエラーが発生しました' }}
-    </p>
-    <NuxtButton @click="goHome">
-      トップに戻る
-    </NuxtButton>
-  </div>
+  <NuxtLayout>
+    <div class="h-full flex flex-col gap-10 items-center justify-center">
+      <h1 class="text-8xl font-bold text-error-500">
+        {{ error?.statusCode }}
+      </h1>
+      <p class="text-xl text-gray-500">
+        {{ error?.statusMessage || '予期せぬエラーが発生しました' }}
+      </p>
+      <NuxtButton @click="goHome">
+        トップに戻る
+      </NuxtButton>
+    </div>
+  </NuxtLayout>
 </template>
 
 <script lang="ts" setup>
