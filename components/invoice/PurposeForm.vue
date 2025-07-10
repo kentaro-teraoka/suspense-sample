0b05cc755cd48e386bd7812a67db303a10662c98
k-teraoka <k-teraoka@labol.co.jp>
2025-07-10 11:39:00 +0900

    suspenseで分割

---
+++ components/invoice/PurposeForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <NuxtFormField label="ご利用目的" name="purpose" size="xl">
+    <NuxtSelect
+      v-model="modelValue"
+      :items="purposeOptions"
+      placeholder="選択してください" />
+  </NuxtFormField>
+</template>
+
+<script lang="ts" setup>
+import { type InvoiceType, fetchPurposeOptions } from '~/api/invoice/history';
+
+const modelValue = defineModel<InvoiceType["purpose"]>()
+
+const purposeOptions = ref<string[]>([])
+
+// ご利用目的選択肢取得
+try {
+  purposeOptions.value = await fetchPurposeOptions();
+} catch (err) {
+  console.error('ご利用目的の取得に失敗しました:', err);
+}
+</script>
--- pages/invoice/form.vue
+++ pages/invoice/form.vue
@@ -1,5 +1,18 @@
 <template>
-  <FormGroup />
+  <div class="space-y-10">
+    <h1 class="text-4xl font-bold">Invoice Form</h1>
+    <Suspense>
+      <FormGroup />
+      <template #fallback>
+        <div class="space-y-6">
+          <div class="space-y-4" v-for="i in 4">
+            <NuxtSkeleton class="w-50 h-8 rounded-md" />
+            <NuxtSkeleton class="w-80 h-12 rounded-md" />
+          </div>
+        </div>
+      </template>
+    </Suspense>
+  </div>
 </template>
 
 <script lang="ts" setup>
